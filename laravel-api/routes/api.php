c359140fd9bf909b9c057d511ad95dac8ab4b18d
julsluks <a22betvilver@inspedralbes.cat>
2023-10-19 11:57:56 +0200

    19/10/2023 min 25:23

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+// Route::get('/task', [TodoController::class, 'index']);
+// Route::post('/task', [TodoController::class, 'store']);
+
+Route::resource("todo", TodoController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
